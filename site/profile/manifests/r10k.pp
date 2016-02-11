700f6debe0c733ccf806bbc3c447463fa0974ceb
Jesse Reynolds <jesse@va.com.au>
2016-02-10 23:16:20 -0800

    add r10k and mco

---
+++ site/profile/manifests/r10k.pp
@@ -0,0 +1,11 @@
+class profile::r10k (
+  $remote = 'https://github.com/jessereynolds/control-repo-pe3.8-r10k.git'
+) {
+
+  class {'r10k':
+    remote => $remote,
+  }
+
+  include 'r10k::mcollective'
+
+}
